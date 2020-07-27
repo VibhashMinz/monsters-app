6de4804baf233cf46e8a115373cc6adc7b6bfa3c
VibhashMinz <vibhashgyanminz23@gmail.com>
2020-07-27 08:32:38 +0530

    Updates

---
+++ precache-manifest.dab1faa19e843e4110034f0ceae976f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e1830c28d4dc01a22f0c6152c09cc30",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "19fb5c8508a5cebab4c1",
+    "url": "/monsters-app/static/css/main.35c7c237.chunk.css"
+  },
+  {
+    "revision": "76e1e33d0dc86b7a1eb3",
+    "url": "/monsters-app/static/js/2.2e94f083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-app/static/js/2.2e94f083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19fb5c8508a5cebab4c1",
+    "url": "/monsters-app/static/js/main.7bab5cfb.chunk.js"
+  },
+  {
+    "revision": "9cd392af60b73ea37e8f",
+    "url": "/monsters-app/static/js/runtime-main.ce4bea50.js"
+  }
+]);
